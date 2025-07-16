c6818115ce0cc1524192bc027edd792a749703a7
Harsh saini <harshsaini555333@gmail.com>
2025-07-16 10:46:14 +0530

    Implement cache Invalidation logic

--- services/cacheService.js
+++ services/cacheService.js
@@ -8,12 +8,31 @@ function set(key, data) {
   return cache[key];
 }
 
+// function get(key){
+//   const cachedData = cache[key];
+//   if(cachedData){
+//     return cachedData.data;
+//   }
+//   return null;
+// }
+
 function get(key){
   const cachedData = cache[key];
-  if(cachedData){
-    return cachedData
+  if(!cachedData) return null;
+  const currentTime = Date.now();
+  const cachedTime = cachedData.timestamp;
+  const maxTime = 1 * 60 * 1000; // 1 minute in milliseconds
+  console.log(currentTime);
+  console.log(cachedTime);
+  console.log(currentTime - cachedTime);
+  console.log(maxTime);
+  
+  if(currentTime - cachedTime > maxTime){
+    delete cache[key];
+    console.log(`Cache expired for key: ${key}`);
+    return null;
   }
-  return null;
+  return cachedData.data;
 }
 
 module.exports = {set, get};
